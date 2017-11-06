cd2e4e604c8b33e577c1ca3db22444896dff02ca
Diego Intra <intradiego@gmail.com>
2017-11-06 21:31:23 +0100

    prova con parametro

--- web/index.php
+++ web/index.php
@@ -24,7 +24,7 @@ $app->get('/', function() use($app) {
 
 // aggiunta parametro
 
-$app->get('/', function() use($app) {
+$app->get('/provaparametro', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return str_repeat('Hello', getenv('TIMES'));
 });
