965b3c04338da63e20bc602cfd56dacd4a13c985
crispy99 <33433412+crispy99@users.noreply.github.com>
2017-11-17 16:42:51 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,14 @@
 <?php
-	$Count['Cookie_accessi']++;
-	setcookie ('Cookie_accessi', $Count['Cookie_accessi']);
+	if(!isset($Count['Cookie_accessi']))
+	{
+		$Count['Cookie_accessi']++;
+		setcookie ('Cookie_accessi', $Count['Cookie_accessi']);
+	}
+	else
+	{
+		$Count['Cookie_accessi']++;
+		setcookie ('Cookie_accessi', $Count['Cookie_accessi']);
+	}
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +24,9 @@
 				echo "Prima volta per te, BRAVOH!!!";
 			}
 			else
+			{
 				echo "Sei l'utente numero ". $Count['Cookie_accessi'] . " che ha fatto l'accesso a questo sito."; 
+			}
 		?>		
 	</body>
 </html>
